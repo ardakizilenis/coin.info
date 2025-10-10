5cf40fc64b9764a00c9f43da1e97802c56162371
Arda Kizilenis <kizilenis@live.de>
2025-10-11 00:48:27 +0200

    Finish the Coins Section and make the API work. The Coins Section now shows the Top50 coins, sorted by Market Cap

--- index.js
+++ index.js
@@ -1,15 +1,41 @@
-import express from "express";
+import express, { response } from "express";
 import ejs from "ejs";
 import axios from "axios";
 
 const app = express();
 const port = 3030;
+const baseUrl = 'https://api.coinranking.com/v2';
+const apiToken = 'coinranking346cf11fbefb313ffedb6e7cf7c883379b422f8336b4bb4d';
 
-app.use(express.static("public"))
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-    res.render("index.ejs");
+    res.redirect("/home");
 })
+
+app.get("/home", (req, res) => {
+    res.render("index.ejs", {
+        currentPage : "index"
+    });
+})
+
+app.get("/coins", (req, res) => {
+    const options = {
+        headers: {
+            'x-access-token': apiToken
+        }
+    };
+    fetch('https://api.coinranking.com/v2/coins', options)
+        .then((response) => response.json())
+        .then((result) => {
+            res.render("coins.ejs", {
+                currentPage : "coins",
+                coinlist : JSON.stringify(result.data.coins)
+            });
+        });
+});
+
+
 
 app.listen(port, () => {
     console.log("Listening on port "+port);
