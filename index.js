97e0dd41860230fc403c09701839c8c263cdb237
Arda Kizilenis <kizilenis@live.de>
2025-10-12 23:26:09 +0200

    finish about page, ready for first release

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 import express, { response } from "express";
+import axios from "axios";
 
 const app = express();
 const port = 3030;
 const baseUrl = 'https://api.coinranking.com/v2';
+const newsApiKey = '8d7d57cbfa004157a69cf209b7339d14';
 const options = {
-        headers: {
-            'x-access-token': 'coinranking346cf11fbefb313ffedb6e7cf7c883379b422f8336b4bb4d'
-        }
-    };
+    headers: {
+        'x-access-token': 'coinranking346cf11fbefb313ffedb6e7cf7c883379b422f8336b4bb4d'
+    }
+};
 
 app.use(express.static("public"));
 
@@ -65,6 +67,23 @@ app.get("/trending", (req, res) => {
     }
 })
 
+app.get("/news", async (req, res) => {
+    console.log(req.query.sortBy)
+    try {
+    const response = await axios.get(`https://newsapi.org/v2/everything?q=Crypto&sortBy=${req.query.sortBy}&language=en&apiKey=${newsApiKey}`);
+    res.render("news.ejs", {
+        currentPage : "news",
+        news: JSON.stringify(response.data)
+    })
+  } catch (e) {
+    console.error(e.message);
+  }
+})
+
+app.get('/about', (req, res) => {
+  res.render('about.ejs', { currentPage: 'about' });
+});
+
 app.get("/search", (req, res) => {
     const query = (req.query.q || "").toLowerCase();
     fetch(`${baseUrl}/search-suggestions?query=${query}`, options)
@@ -88,7 +107,6 @@ app.get("/coinpage/:uuid", (req, res) => {
                 coin : JSON.stringify(result.data.coin)
             })
         });
-
 })
 
 app.listen(port, () => {
