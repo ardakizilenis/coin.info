444a6a9eff6272ab72c3f11a9e7d883a24a3c493
Arda Kizilenis <kizilenis@live.de>
2025-10-12 03:11:38 +0200

    Individual Coin Page, Searching Functionality and Top-50 CTA implemented

--- index.js
+++ index.js
@@ -1,41 +1,72 @@
 import express, { response } from "express";
-import ejs from "ejs";
-import axios from "axios";
 
 const app = express();
 const port = 3030;
 const baseUrl = 'https://api.coinranking.com/v2';
-const apiToken = 'coinranking346cf11fbefb313ffedb6e7cf7c883379b422f8336b4bb4d';
+const options = {
+        headers: {
+            'x-access-token': 'coinranking346cf11fbefb313ffedb6e7cf7c883379b422f8336b4bb4d'
+        }
+    };
 
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-    res.redirect("/home");
-})
-
-app.get("/home", (req, res) => {
     res.render("index.ejs", {
         currentPage : "index"
     });
 })
 
 app.get("/coins", (req, res) => {
-    const options = {
-        headers: {
-            'x-access-token': apiToken
-        }
-    };
-    fetch('https://api.coinranking.com/v2/coins', options)
+    const query = (req.query.q || "").toLowerCase();
+    if(query) {
+        fetch(`${baseUrl}/search-suggestions?query=${query}&limit=5`, options)
+            .then((response) => response.json())
+            .then((result) => {
+                res.render("search.ejs", {
+                    currentPage : "search",
+                    coinlist : JSON.stringify(result.data.coins)
+                });
+                console.log(result.data.coins);
+            })
+    }
+    else{
+        fetch(`${baseUrl}/coins?limit=100`, options)
+            .then((response) => response.json())
+            .then((result) => {
+                res.render("coins.ejs", {
+                    currentPage : "coins",
+                    coinlist : JSON.stringify(result.data.coins)
+                });
+            });
+    }
+})
+
+app.get("/search", (req, res) => {
+    const query = (req.query.q || "").toLowerCase();
+    fetch(`${baseUrl}/search-suggestions?query=${query}`, options)
+            .then((response) => response.json())
+            .then((result) => {
+                res.render("search.ejs", {
+                    currentPage : "search",
+                    coinlist : JSON.stringify(result.data.coins)
+                });
+                console.log(result.data.coins);
+            })
+})
+
+app.get("/coinpage/:uuid", (req, res) => {
+    fetch(`${baseUrl}/coin/${req.params.uuid}`, options)
         .then((response) => response.json())
         .then((result) => {
-            res.render("coins.ejs", {
-                currentPage : "coins",
-                coinlist : JSON.stringify(result.data.coins)
-            });
+            console.log(result.data.coin);
+            res.render("coinpage.ejs", {
+                currentPage : "coinpage",
+                coin : JSON.stringify(result.data.coin)
+            })
         });
-});
 
-
+})
 
 app.listen(port, () => {
     console.log("Listening on port "+port);
